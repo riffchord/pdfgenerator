6137c7a38d8238cf18e6f25ef3ac977e8b2bacc6
riffchord <33197481+riffchord@users.noreply.github.com>
2024-01-24 21:18:12 +0100

    port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 666;
 
 // Body Parser Middleware
 app.use(bodyParser.json());
